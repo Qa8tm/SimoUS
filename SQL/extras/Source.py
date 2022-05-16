8ab66ef6491bec66b2c0559db2ef16ed868c2a8a
Qa8tm <102294427+Qa8tm@users.noreply.github.com>
2022-05-16 11:40:16 +0300

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -8,4 +8,9 @@ help1 = ("**☭⦙ كيفيه التنصيب :**")
 help2 = ("**☭⦙ قـائمـه الاوامـر :**\n**☭⦙ قنـاه السـورس :** @ADWSL** ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**.𓄌 : version 7.5  𓇡.** \n.𓄌 : me  {mention}  𓇡. \n**.𓄌 : time  {TM}  𓇡.**\n**.𓄌 : My Bot {TG_BOT} 𓇡.**\n**.𓄌 : Source : @ADWSL  𓇡.**"
+Sour = f"""
+**.𓄌 : version 1  𓇡.
+**.𓄌 : me  {mention}  𓇡. 
+**.𓄌 : time  {TM}  𓇡.**
+**.𓄌 : My Bot {TG_BOT} 𓇡.**
+**.𓄌 : Source : @ADWSL  𓇡.**"""
